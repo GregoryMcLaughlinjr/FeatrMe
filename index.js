03636a58dac74b9a507c416ea2b937d208c88412
Gregory McLaughlin Jr <112370571+GregoryMcLaughlinjr@users.noreply.github.com>
2022-11-14 12:25:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function recordScratch() {
+  let audio = new Audio('audio/soundEffects/scratch.wav');
+audio.play();
+}
+
+function cashRegister() {
+  let audio = new Audio('audio/soundEffects/Cash Register.mp3');
+audio.play();
+}
